Codiscope-public/quorum

Move raft checkpoint logging to logger package.
This allows us to use it everywhere and turn it on / off from command
flags.

internal/ethapi/api.go
@@ -21,7 +21,6 @@ import (
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
-	"log"
 	"math/big"
 	"net/http"
 	"strings"
@@ -1194,12 +1193,10 @@ func submitTransaction(ctx context.Context, b Backend, tx *types.Transaction, si
 		from, _ := signedTx.From()
 		addr := crypto.CreateAddress(from, signedTx.Nonce())
 		glog.V(logger.Info).Infof("Tx(%s) created: %s\n", signedTx.Hash().Hex(), addr.Hex())
-		// XXX(joel) use logCheckpoint
-		log.Printf("RAFT-CHECKPOINT TX-CREATED (%v, %v)\n", signedTx.Hash().Hex(), addr.Hex())
+		logger.LogRaftCheckpoint(logger.TxCreated, signedTx.Hash().Hex(), addr.Hex())
 	} else {
 		glog.V(logger.Info).Infof("Tx(%s) to: %s\n", signedTx.Hash().Hex(), tx.To().Hex())
-		// XXX(joel) use logCheckpoint
-		log.Printf("RAFT-CHECKPOINT TX-CREATED (%v, %v)\n", signedTx.Hash().Hex(), tx.To().Hex())
+		logger.LogRaftCheckpoint(logger.TxCreated, signedTx.Hash().Hex(), tx.To().Hex())
 	}
 
 	return signedTx.Hash(), nil

raft/handler.go
@@ -349,10 +349,10 @@ func (pm *ProtocolManager) handleRoleChange(roleC <-chan interface{}) {
 			}
 
 			if intRole == minterRole {
-				logCheckpoint(becameMinter, "")
+				logger.LogRaftCheckpoint(logger.BecameMinter)
 				pm.minter.start()
 			} else { // verifier
-				logCheckpoint(becameVerifier, "")
+				logger.LogRaftCheckpoint(logger.BecameVerifier)
 				pm.minter.stop()
 			}
 
@@ -604,10 +604,6 @@ func sleep(duration time.Duration) {
 	<-time.NewTimer(duration).C
 }
 
-func logCheckpoint(checkpointName string, iface interface{}) {
-	glog.V(logger.Info).Infof("RAFT-CHECKPOINT %s %v\n", checkpointName, iface)
-}
-
 func blockExtendsChain(block *types.Block, chain *core.BlockChain) bool {
 	return block.ParentHash() == chain.CurrentBlock().Hash()
 }
@@ -627,7 +623,7 @@ func (pm *ProtocolManager) applyNewChainHead(block *types.Block) {
 		}
 
 		for _, tx := range block.Transactions() {
-			logCheckpoint(txAccepted, tx.Hash().Hex())
+			logger.LogRaftCheckpoint(logger.TxAccepted, tx.Hash().Hex())
 		}
 
 		if pm.blockchain.HasBlock(block.Hash()) {

joelburget/go-ethereum

internal/ethapi/api.go
@@ -1170,8 +1170,10 @@ func submitTransaction(ctx context.Context, b Backend, tx *types.Transaction) (c
 		from, _ := types.Sender(signer, tx)
 		addr := crypto.CreateAddress(from, tx.Nonce())
 		log.Info("Submitted contract creation", "fullhash", tx.Hash().Hex(), "contract", addr.Hex())
+		log.LogRaftCheckpoint(log.TxCreated, tx.Hash().Hex(), addr.Hex())
 	} else {
 		log.Info("Submitted transaction", "fullhash", tx.Hash().Hex(), "recipient", tx.To())
+		log.LogRaftCheckpoint(log.TxCreated, tx.Hash().Hex(), tx.To().Hex())
 	}
 	return tx.Hash(), nil
 }

raft/handler.go
@@ -386,10 +386,10 @@ func (pm *ProtocolManager) handleRoleChange(roleC <-chan interface{}) {
 			}
 
 			if intRole == minterRole {
-				logCheckpoint(becameMinter, "")
+				log.LogRaftCheckpoint(log.BecameMinter)
 				pm.minter.start()
 			} else { // verifier
-				logCheckpoint(becameVerifier, "")
+				log.LogRaftCheckpoint(log.BecameVerifier)
 				pm.minter.stop()
 			}
 
@@ -649,10 +649,6 @@ func sleep(duration time.Duration) {
 	<-time.NewTimer(duration).C
 }
 
-func logCheckpoint(checkpointName string, iface interface{}) {
-	log.Info("RAFT-CHECKPOINT", "name", checkpointName, "data", iface)
-}
-
 func blockExtendsChain(block *types.Block, chain *core.BlockChain) bool {
 	return block.ParentHash() == chain.CurrentBlock().Hash()
 }
@@ -672,7 +668,7 @@ func (pm *ProtocolManager) applyNewChainHead(block *types.Block) {
 		}
 
 		for _, tx := range block.Transactions() {
-			logCheckpoint(txAccepted, tx.Hash().Hex())
+			log.LogRaftCheckpoint(log.TxAccepted, tx.Hash().Hex())
 		}
 
 		_, err := pm.blockchain.InsertChain([]*types.Block{block})